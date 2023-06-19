d8889cd0f392343e554537eb45608305800983dd
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2023-06-19 12:07:27 -0300

    Project Created

---
+++ src/main/java/com/devsuperior/challengcrud/ChallengCrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.challengcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/challengcrud/ChallengCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.challengcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
